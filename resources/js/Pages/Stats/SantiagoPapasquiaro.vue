3a8a61f7741060df826eff7a9ce934042bd56074
Alejandro Parra <alejandro.parra@iepcdurango.mx>
2024-05-28 14:54:05 -0600

    Textos

--- resources/js/Pages/Stats/SantiagoPapasquiaro.vue
+++ resources/js/Pages/Stats/SantiagoPapasquiaro.vue
@@ -57,32 +57,32 @@ export default {
         <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
             <div class="grid md:grid-cols-2 sm:grid-cols-1 lg:md:grid-cols-3 gap-4 mb-4">
                 <div
-                    class="flex flex-col lg:flex-row w-full items-center border border-gray-200 px-3 py-2 rounded shadow-lg">
-                    <circle-chart ref="msc" :series="[materialSupplied.series.progress]" :width="300"
-                                  :key="2"></circle-chart>
+                    class="flex flex-col w-full items-center border border-gray-200 px-3 py-2 rounded shadow-lg">
                     <div class="shrink w-full md:ml-3 grid grid-cols-1 justify-center text-center lg:text-left">
-                        <p class="font-bold text-gray-500">Entrega de Material</p>
+                        <p class="font-bold text-gray-500">Entrega de Documentación y Material Electoral a CAEL/SEL</p>
                         <p class="font-medium text-gray-400">{{ materialSupplied.series.received }} de
                             {{ materialSupplied.series.expected }}</p>
                     </div>
+                    <circle-chart ref="msc" :series="[materialSupplied.series.progress]" :width="300"
+                                  :key="2"></circle-chart>
                 </div>
                 <div
-                    class="flex flex-col lg:flex-row w-full items-center border border-gray-200 px-3 py-2 rounded shadow-lg">
-                    <circle-chart :series="[packetsReceived.series.progress]" :width="300" :key="1"></circle-chart>
+                    class="flex flex-col w-full items-center border border-gray-200 px-3 py-2 rounded shadow-lg">
                     <div class="shrink w-full md:ml-3 grid grid-cols-1 justify-center text-center lg:text-left">
-                        <p class="font-bold text-gray-500">Paquetes recibidos</p>
+                        <p class="font-bold text-gray-500">Paquetes recibidos en CME (Jornada Electoral)</p>
                         <p class="font-medium text-gray-400">{{ packetsReceived.series.received }} de
                             {{ packetsReceived.series.expected }}</p>
                     </div>
+                    <circle-chart :series="[packetsReceived.series.progress]" :width="300" :key="1"></circle-chart>
                 </div>
                 <div
-                    class="flex flex-col lg:flex-row w-full items-center border border-gray-200 px-3 py-2 rounded shadow-lg">
-                    <circle-chart :series="[aecRegistration.series.progress]" :width="300" :key="3"></circle-chart>
+                    class="flex flex-col w-full items-center border border-gray-200 px-3 py-2 rounded shadow-lg">
                     <div class="shrink w-full md:ml-3 grid grid-cols-1 justify-center text-center lg:text-left">
                         <p class="font-bold text-gray-500">AEC registradas</p>
                         <p class="font-medium text-gray-400">{{ aecRegistration.series.received }} de
                             {{ aecRegistration.series.expected }}</p>
                     </div>
+                    <circle-chart :series="[aecRegistration.series.progress]" :width="300" :key="3"></circle-chart>
                 </div>
             </div>
         </div>
