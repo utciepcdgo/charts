974eff1dde8663f5230e770daa07d52bae616b40
Alejandro Parra <alejandro.parra@iepcdurango.mx>
2024-06-05 15:13:25 -0600

    Iconos

--- resources/js/Pages/Stats/PuebloNuevo.vue
+++ resources/js/Pages/Stats/PuebloNuevo.vue
@@ -108,14 +108,13 @@ export default {
                                 </h5>
                                 <Link @click.prevent="Files.getCollatedRecountPacketsList(2)" href="#"
                                       class="inline-flex space-x-2 items-center text-sm font-medium text-center text-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                         stroke-linecap="round" stroke-linejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-type-csv">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                        <path d="M12 20h-6a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12v5"/>
-                                        <path d="M13 16h-7a2 2 0 0 0 -2 2"/>
-                                        <path d="M15 19l3 3l3 -3"/>
-                                        <path d="M18 22v-9"/>
+                                        <path d="M14 3v4a1 1 0 0 0 1 1h4"/>
+                                        <path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4"/>
+                                        <path d="M7 16.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0"/>
+                                        <path d="M10 20.25c0 .414 .336 .75 .75 .75h1.25a1 1 0 0 0 1 -1v-1a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-1a1 1 0 0 1 1 -1h1.25a.75 .75 0 0 1 .75 .75"/>
+                                        <path d="M16 15l2 6l2 -6"/>
                                     </svg>
                                     <span>Descargar</span>
                                 </Link>
@@ -137,14 +136,13 @@ export default {
                                 </h5>
                                 <Link @click.prevent="Files.getCollatedRecountPacketsList(1)" href="#"
                                       class="inline-flex space-x-2 items-center text-sm font-medium text-center text-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                         stroke-linecap="round" stroke-linejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-type-csv">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                        <path d="M12 20h-6a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12v5"/>
-                                        <path d="M13 16h-7a2 2 0 0 0 -2 2"/>
-                                        <path d="M15 19l3 3l3 -3"/>
-                                        <path d="M18 22v-9"/>
+                                        <path d="M14 3v4a1 1 0 0 0 1 1h4"/>
+                                        <path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4"/>
+                                        <path d="M7 16.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0"/>
+                                        <path d="M10 20.25c0 .414 .336 .75 .75 .75h1.25a1 1 0 0 0 1 -1v-1a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-1a1 1 0 0 1 1 -1h1.25a.75 .75 0 0 1 .75 .75"/>
+                                        <path d="M16 15l2 6l2 -6"/>
                                     </svg>
                                     <span>Descargar</span>
                                 </Link>
